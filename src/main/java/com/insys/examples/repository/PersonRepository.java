b455d86486a511b1236e3905cb9d31074d968ac0
Brad Starkenberg <brad@starkenberg.net>
2016-02-03 22:33:31 -0500

    Add Repository and Entity

---
+++ src/main/java/com/insys/examples/repository/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.insys.examples.repository;
+
+/**
+ * @author Brad Starkenberg
+ */
+import java.util.List;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.insys.examples.model.Person;
+
+@RepositoryRestResource(collectionResourceRel = "people", path = "people")
+public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
+
+	List<Person> findByLastName(@Param("name") String name);
+
+}
