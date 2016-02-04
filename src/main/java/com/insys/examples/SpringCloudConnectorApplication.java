7ab64c471ddd4159a80b5a5a57d7d066c8775733
Starkenberg <bstarkenberg@NJ395MAC06.local>
2016-02-03 21:57:23 -0500

    Initial Commit

---
+++ src/main/java/com/insys/examples/SpringCloudConnectorApplication.java
@@ -0,0 +1,12 @@
+package com.insys.examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConnectorApplication.class, args);
+	}
+}
---
+++ src/test/java/com/insys/examples/SpringCloudConnectorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.insys.examples;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringCloudConnectorApplication.class)
+public class SpringCloudConnectorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
